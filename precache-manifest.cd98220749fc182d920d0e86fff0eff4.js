896156283cff18a46dea1f89d3a84b0ad9b2004d
Jiaxin Deng <ash.@Jiaxins-MacBook-Pro-2.local>
2020-09-29 00:53:03 +0800

    Updates

---
+++ precache-manifest.cd98220749fc182d920d0e86fff0eff4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b43e437660f41a7326bb08c125923a2",
+    "url": "/GlucoseMeter/index.html"
+  },
+  {
+    "revision": "c6540eac81c0f867bf6b",
+    "url": "/GlucoseMeter/static/css/main.e9c2f9b8.chunk.css"
+  },
+  {
+    "revision": "473dce11750168fb3500",
+    "url": "/GlucoseMeter/static/js/2.9b6b5808.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/GlucoseMeter/static/js/2.9b6b5808.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6540eac81c0f867bf6b",
+    "url": "/GlucoseMeter/static/js/main.c9e548a8.chunk.js"
+  },
+  {
+    "revision": "00b5f25e4e1f0548e9bb",
+    "url": "/GlucoseMeter/static/js/runtime-main.f5d33258.js"
+  }
+]);
